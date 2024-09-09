a11fd299631aafb171739ef454a51ffc3968b5ad
= <=>
2024-09-08 20:05:51 -0400

    html cambiado, logos

--- website/src/components/Helmet.jsx
+++ website/src/components/Helmet.jsx
@@ -6,7 +6,7 @@ class PageHelmet extends Component{
         return(
             <React.Fragment>
                 <Helmet>
-                    <title>{this.props.pageTitle} || React Multipurpose Template </title>
+                    <title>{this.props.pageTitle}</title>
                     <meta name="description" content="Space Marketing es una agencia de Marketing Digital que ofrece direferentes servicios tales como Desarrollo Web, Desarrollo de Presencia Online, Logos, Cursos de Marketing, Manejo de Redes sociales, Edición de videos" />
                 </Helmet>
             </React.Fragment>
